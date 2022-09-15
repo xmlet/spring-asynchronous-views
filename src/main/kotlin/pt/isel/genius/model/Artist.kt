dee6382440422e1552249f2aca583d391a3e16fd
Miguel Gamboa <miguelgamboa@outlook.com>
2022-09-15 16:28:06 +0100

    Thymeleaf view and handler for reactive approach from Artist publishers.

--- src/main/kotlin/pt/isel/genius/model/Artist.kt
+++ src/main/kotlin/pt/isel/genius/model/Artist.kt
@@ -1,5 +1,6 @@
 package pt.isel.genius.model
 
+import reactor.core.publisher.Mono
 import java.util.concurrent.CompletableFuture
 
 data class Artist(private val allMusic: AllMusicArtist, private val spotify: SpotifyArtist, private val apple: AppleMusicArtist) {
@@ -11,6 +12,15 @@ data class Artist(private val allMusic: AllMusicArtist, private val spotify: Spo
 
     val cfApple
         get() = CompletableFuture.completedFuture(apple).delay(2000)
+
+    val pubAllMusicArtist
+        get() = Mono.fromFuture(cfAllMusicArtist).flux()
+
+    val pubSpotify
+        get() = Mono.fromFuture(cfSpotify).flux()
+
+    val pubApple
+        get() = Mono.fromFuture(cfApple).flux()
 }
 
 
--- src/main/kotlin/pt/isel/genius/thymeleaf/ArtistControllerThymeleaf.kt
+++ src/main/kotlin/pt/isel/genius/thymeleaf/ArtistControllerThymeleaf.kt
@@ -5,6 +5,7 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable
 import pt.isel.genius.artists
 import pt.isel.genius.model.Artist
 
@@ -23,5 +24,22 @@ class ArtistControllerThymeleaf {
         model.addAttribute("spotify", artist.cfSpotify.join())
         model.addAttribute("apple", artist.cfApple.join())
         return "artist"
+    }
+
+    @GetMapping("/reactive/artist/{name}")
+    fun reactiveHandlerArtist(@PathVariable("name") name: String, model: Model): String {
+        val artist: Artist = requireNotNull(artists[name.lowercase()]) {
+            "No resource for artist name $name"
+        }
+        model.addAttribute("startTime", System.currentTimeMillis())
+        model.addAttribute("artist", name)
+        /**
+         * !!! Only one data-driver variable is allowed to be specified as a model attribute!!!!
+         * Otherwise, it causes TemplateProcessingException.
+         */
+        // model.addAttribute("allMusic", ReactiveDataDriverContextVariable(artist.pubAllMusicArtist, 1))
+        // model.addAttribute("spotify", ReactiveDataDriverContextVariable(artist.pubSpotify, 1))
+        model.addAttribute("apple", ReactiveDataDriverContextVariable(artist.pubApple, 1))
+        return "artistReactive"
     }
 }
